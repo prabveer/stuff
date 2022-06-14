4d581901f8922fe69f2a9d238c1d478fd0c9bde5
prabveer <psp180001@utdallas.edu>
2022-06-14 15:16:37 -0500

    a

--- C++/LargestPalindrome.cpp
+++ C++/LargestPalindrome.cpp
@@ -7,16 +7,21 @@ using namespace std;
 
 // Simple Program that gets the largest palindrome of 3 digit numbers //
 // Prabveer Pannu //
+// simple check that checks of the number is a palindrome or not //
 bool palibdromeCheck(string num)
 {
+    // set a string that will be = to num reversed
     string reversednum = num;
+    // reverse num //
     reverse(num.begin(), num.end());
+    // check if reversed num is equal to the original num //
     if(reversednum == num)
         return true;
     return false;
 }
 int main()
 {   
+    // nested loop that will go through all the numbers to find the largest palindrome //
     int number1 = 999, number2 = 999, max = 0, firstmulti, secondmulti;
     for(int i = 999; i > 100; i--)
     {
@@ -24,8 +29,10 @@ int main()
         for(int x = i; x > 100; x--)
         {
             string sum = to_string(x * i);
+            // checks if something is a palindrom or not //
             if(palibdromeCheck(sum))
             {
+                // checks if the current plaindrom is larger than the on we have set as the largest palindrom so far //
                 if(stoi(sum) > max )
                 {
                     max = stoi(sum);
@@ -38,6 +45,7 @@ int main()
         }
     }
 
+    // outputing results //
     cout << "The largest plaindrome it the number " << firstmulti << " times the number " << secondmulti << " which equals: " << max << endl;
     return 0;
 
